f772d18715af228e990955a31652f82c05416b2a
jihuen <23026390@myrp.edu.sg>
2025-01-21 10:17:01 +0800

    /

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const diplomas = [
+    {name: "Diploma in Applied AI & Analytics",
+    id: "diploma-in-applied-ai-&-analytics",
+    courses:[
+        {name: "AI Essentials and Innovations", 
+        id: "ai-essentials-and-innovations"},
+        {name: "Data Analytics with GenAI", 
+        id: "data-analytics-with-genai"},
+    ]},
+
+]
